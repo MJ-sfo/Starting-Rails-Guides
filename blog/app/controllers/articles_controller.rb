754356a922128a85a0b9f3131c45f5702cc2f378
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-18 22:56:17 -0700

    Article creates table for data

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,6 +3,11 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect    
+    # render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+    # using Cap - referring to class Article, defined in app/models/article.rb
+
+    @article.save  # returns boolean - whether the article was saved
+    redirect_to @article
   end
 end
